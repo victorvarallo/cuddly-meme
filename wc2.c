106759fa69f369691ca6ae572065aefa6395c80b
VICTOR VARALLO FERREIRA <16104092@l1801265.portoalegre.pucrsnet.br>
2016-05-18 11:31:27 -0300

    contador palavras ok

--- wc2.c
+++ wc2.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 
+
 int main() {
 
 	FILE *arquivo;
 	   
     char arqn[51];
-    int c;    
+    int c,i;    
     int cl = 0;
     int cb = 0;
     int cp = 0;
 
-    printf("nome do arquivo(com extensao):  ");
-	scanf("%s",arqn);       //ler arquivo
+    
 
-	arquivo = fopen(arqn, "r");    //abrir arquivo digitado
+    printf("nome do arquivo(com extensao):  ");
+	scanf("%s",arqn);                         //ler arquivo
+
+	arquivo = fopen(arqn, "r");               //abrir arquivo digitado
 
 	c = getc(arquivo);
     while( c != -1 )
     {
-        cb++;
-        if (c == '\n')
-            cl = cl + 1;
-        if (c == ' ')
-            cp = cp + 1;
+        cb++;                                 //contador de bytes
 
-		
+        if (c == '\n')
+            cl++;                             //contador de linhas
+
+        if (c == ' ' || c=='\t' || c=='\n')
+        {
+            i=0;
+		}
+        else    i++;
+        if(i==1)
+            cp++;                              //contador de palavras
+        
+        
+
 		c = getc(arquivo);
 	}
 
