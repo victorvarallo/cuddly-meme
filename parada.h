a6c607d7a950c6ddff5880b54a0eb5af43f6f76d
victorvarallo <victor.varallo@acad.pucrs.br>
2016-09-07 12:24:40 -0300

    Add files via upload
    tp1

---
+++ parada.h
@@ -0,0 +1,12 @@
+#ifndef __PARADA_H__
+#define __PARADA_H__
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class listaveiculo{
+
+};
+#endif
---
+++ tp1/listaveiculo.h
@@ -0,0 +1,12 @@
+#ifndef __LISTAVEICULO_H__
+#define __LISTAVEICULO_H__
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class listaveiculo{
+
+};
+#endif
